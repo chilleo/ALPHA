165fcb6066705a2efac2bf1deee10dd7e1d3104c
Peter Dulworth <pdulworth97@gmail.com>
2018-03-21 13:37:23 -0500

    cross platform

--- setup.py
+++ setup.py
@@ -1,14 +1,29 @@
 """
-Setup script for py2app deployment.
+py2app/py2exe build script for ALPHA.
 
-Usage:
+Will automatically ensure that all build prerequisites are available 
+via ez_setup.
+
+Usage (Mac OS X):
     python setup.py py2app
+
+Usage (Windows):
+    python setup.py py2exe
 """
 
+from ez_setup import use_setuptools
+use_setuptools()
+
+import sys
 from setuptools import setup
 
 APP = ['source/main.py']
-DATA_FILES = []
+DATA_FILES = [
+	'imgs/alphaLogo.png',
+	'imgs/LStatisticTree.png',
+	'imgs/warning.png',
+	'imgs/tree.png',
+]
 PACKAGES = [
 	'sip', 
 	'PyQt4._qt', 
@@ -39,14 +54,52 @@ PACKAGES = [
     'biopython'
  ]
 OPTIONS = {
-	'iconfile': '/Users/Peter/PycharmProjects/ALPHA/browser.icns', 
+	'iconfile': 'alphaLogo.icns', 
 	'argv_emulation': True, 
 	'includes': PACKAGES
 }
-
+# if sys.platform == 'darwin':
+	# pass
+# elif sys.platform = 'win32'
 setup(
+	name='ALPHA',
+	version='1.0',
+	description='Automated Local Phylogenomic Analyses',
+	url='https://github.com/chilleo/ALPHA',
+	author='Chabrielle Allen, Travis Benedict, Ryan Elworth, Peter Dulworth',
+	author_email='chileo@gmail.com',
+	license='MIT',
     app=APP,
     data_files=DATA_FILES,
     options={'py2app': OPTIONS},
+    install_requires=['pillow'],
     setup_requires=['py2app'],
 )
+
+
+#  mainscript = 'MyApplication.py'
+
+#  if sys.platform == 'darwin':
+#      extra_options = dict(
+#          setup_requires=['py2app'],
+#          app=[mainscript],
+#          # Cross-platform applications generally expect sys.argv to
+#          # be used for opening files.
+#          options=dict(py2app=dict(argv_emulation=True)),
+#      )
+#  elif sys.platform == 'win32':
+#      extra_options = dict(
+#          setup_requires=['py2exe'],
+#          app=[mainscript],
+#      )
+# else:
+#      extra_options = dict(
+#          # Normally unix-like platforms will use "setup.py install"
+#          # and install the main script as such
+#          scripts=[mainscript],
+#      )
+
+# setup(
+#     name="MyApplication",
+#     **extra_options
+# )
